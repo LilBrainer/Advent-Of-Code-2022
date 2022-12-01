9f053d858dda5f0504f72dc1eef33df7dc2a9e85
Jose <jose.puy.7e6@itb.cat>
2022-12-01 17:38:36 +0100

    Dia uno terminado

--- 2022-12-1/Program.cs
+++ 2022-12-1/Program.cs
@@ -7,8 +7,100 @@ namespace _2022_12_1
         static void Main(string[] args)
         {
             Program pr = new Program();
+            int resposta;
 
-            Console.WriteLine("Quina part vols?\n\n 1- Part 1 \n 2- Part 2");
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Quina part vols?\n\n 0- Sortir \n 1- Part 1 \n 2- Part 2");
+                resposta = Convert.ToInt32(Console.ReadLine());
+
+                if (resposta == 1)
+                {
+                    parte1();
+                }
+                if (resposta == 2)
+                {
+                    parte2();
+                }
+
+            } while (resposta != 0);
+
+            Console.ReadLine();
+        }
+
+        static void parte1()
+        {
+            Console.Clear();
+
+
+            int maxelf = 0;
+            int elf = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\Usuario\Desktop\Repositorios GITLAB\Advent Of\input\input.txt"))
+            {
+                if (line == "")
+                {
+                    if (elf > maxelf)
+                    {
+                        maxelf = elf;
+                    }
+                    elf = 0;
+                }
+                else
+                {
+                    elf += Convert.ToInt32(line);
+                }
+            }
+            Console.WriteLine(maxelf);
+
+            Console.ReadLine();
+        }
+
+        static void parte2()
+        {
+            Console.Clear();
+
+            int maxelf1 = 0;
+            int maxelf2 = 0;
+            int maxelf3 = 0;
+            int elf = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\Usuario\Desktop\Repositorios GITLAB\Advent Of\input\input.txt"))
+            {
+                if (line == "")
+                {
+                    if (elf > maxelf3)
+                    {
+                        if (elf > maxelf2)
+                        {
+                            if (elf > maxelf1)
+                            {
+                                maxelf3 = maxelf2;
+                                maxelf2 = maxelf1;
+                                maxelf1 = elf;
+                            }
+                            else
+                            {
+                                maxelf3 = maxelf2;
+                                maxelf2 = elf;
+                            }
+                        }
+                        else
+                        {
+                            maxelf3 = elf;
+                        }
+                    }
+                    elf = 0;
+                }
+                else
+                {
+                    elf += Convert.ToInt32(line);
+                }
+            }
+            Console.WriteLine(maxelf1 + maxelf2 + maxelf3);
+
+
 
             Console.ReadLine();
         }
